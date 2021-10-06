973103eac8bf627250c449e8d68acad340a678fb
LYSIOW1231 <92012610+LYSIOW1231@users.noreply.github.com>
2021-10-06 17:37:47 +0800

    Update AVERAGE HEIGHT.py
    Getting average of a row of number from a no heading text file.

--- AVERAGE HEIGHT.py
+++ AVERAGE HEIGHT.py
@@ -1,4 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv('Hap_Seng_ECW_M1_F1')
-df.info()
+df = pd.read_csv('Hap Seng_M3_F27.txt', delimiter='\t')
+#print(df)
+df.columns=['1','2','3','4','5','6','7','8','X','Y','Z','12']
+#print(df['Z'])
+height = df['Z']
+#print(len(height))
+total=sum(df['Z'])
+#print(total)
+
+height = [i for i in height if i !=0]
+#print(height)
+amount=len(height)
+#print(amount)
+
+average= total/amount
+print(average)
+
